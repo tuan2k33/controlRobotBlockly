e297c64549dbc47f230d994704b110a1eb308b27
bachshadow <131352938+bachshadow@users.noreply.github.com>
2024-05-26 10:22:15 +0700

    Update custom_Blocks.js

--- custom_Blocks.js
+++ custom_Blocks.js
@@ -2,39 +2,30 @@ import Blockly from "blockly";
 //import { javascriptGenerator, Order } from "blockly/javascript";
 import { pythonGenerator, Order } from "blockly/python"
 
-Blockly.Blocks["move_forward"] = {
+
+Blockly.Blocks["move"] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("Move Forward");
+    this.appendDummyInput("direction")
+    	.appendField("direction")
+    	.appendField(new Blockly.FieldDropdown([["up","up"],["down","down"]]),"direction_value");
+    this.appendDummyInput("distance")
+    	.appendField("distance")
+    	.appendField(new Blockly.FieldNumber(0),"distance_value");
+    this.appendDummyInput("distance")
+    	.appendField("time")
+    	.appendField(new Blockly.FieldNumber(0),"time_value");
     this.setPreviousStatement(true,null);
-    this.setColour(230);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
-};
-Blockly.Blocks["move_backward"] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Move Backward");
-    this.setPreviousStatement(true,null);
+    this.setNextStatement(true,null);
     this.setColour(250);
     this.setTooltip('');
     this.setHelpUrl('');
   }
 };
-
-
-Blockly.Blocks["migration"] = {
+Blockly.Blocks["rotate"] = {
   init: function() {
     this.appendDummyInput("angle")
     	.appendField("angle")
-    	.appendField(new Blockly.FieldAngle(90),"angle_value")
-    this.appendDummyInput("direction")
-    	.appendField("direction")
-    	.appendField(new Blockly.FieldDropdown([["move_forward","forward"],["move_backward","backward"]]),"direction_value");
-    this.appendDummyInput("distance")
-    	.appendField("distance")
-    	.appendField(new Blockly.FieldNumber(0),"distance_value");
+    	.appendField(new Blockly.FieldAngle(0),"angle_value")
     this.setPreviousStatement(true,null);
     this.setNextStatement(true,null);
     this.setColour(250);
@@ -43,21 +34,15 @@ Blockly.Blocks["migration"] = {
   }
 };
 
-
-pythonGenerator.forBlock["move_forward"] = function (block, generator) {
-  // Tạo mã Python tương ứng với hành động di chuyển robot về phía trước
-  var code = 'move_forward';
-  return code;
-};
-pythonGenerator.forBlock["move_backward"] = function (block, generator) {
-  // Tạo mã Python tương ứng với hành động di chuyển robot về phía sau
-  var code = 'move_backward';
-  return code;
-};
-pythonGenerator.forBlock["migration"] = function (block, generator) {
-  var value_angle = block.getFieldValue('angle_value');
+pythonGenerator.forBlock["move"] = function (block, generator) {
+  var value_time = block.getFieldValue('time_value');
   var value_direction = block.getFieldValue('direction_value');
   var value_distance = block.getFieldValue('distance_value');
-  var code = 'robot.migration( Direction: ' + value_direction + ', Angel: ' + value_angle + ', Distance: ' + value_distance + ');\n';
+  var code = 'robot.move( Direction: ' + value_direction + ', Distance: ' + value_distance + ', Time: ' + value_time + ');\n';
+  return code;
+};
+pythonGenerator.forBlock["rotate"] = function (block, generator) {
+  var value_angle = block.getFieldValue('angle_value');
+  var code = 'robot.rotate( Angel: ' + value_angle + ');\n';
   return code;
 };
