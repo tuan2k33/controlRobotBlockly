f3aec7a58211674da097493b520fbb0a75bb8cd0
Tuan Nguyen <153191880+tuan2k33@users.noreply.github.com>
2024-06-02 12:13:27 +0700

    Update publisher_member_function.py

--- Backend/ros2_ws/py_pubsub/py_pubsub/publisher_member_function.py
+++ Backend/ros2_ws/py_pubsub/py_pubsub/publisher_member_function.py
@@ -20,15 +20,8 @@ from geometry_msgs.msg import Twist
 moveBindings = {
     'up': (1, 0, 0, 0),
     'down': (-1, 0, 0, 0),
-    'rotate_left': (0, 0, 0, 1),
-    'rotate_right': (0, 0, 0, -1),
-    'o': (1, 0, 0, -1),
-    'u': (1, 0, 0, 1),
-    ',': (-1, 0, 0, 0),
-    '.': (-1, 0, 0, 1),
-    'm': (-1, 0, 0, -1),
-    't': (0, 0, 1, 0),
-    'b': (0, 0, -1, 0),
+    'left': (0, 0, 0, 1),
+    'right': (0, 0, 0, -1),
 }
 class MinimalPublisher(Node):
 
@@ -36,33 +29,34 @@ class MinimalPublisher(Node):
         super().__init__('minimal_publisher')
         self.flag=True
         self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 30)
-        timer_period = 1  # seconds
         self.direction=direction
+        
+        #self.distance=distance
+        #self.time=time
+        
         self.sending_request()
         
         #rclpy.shutdown()
         # self.flag=True
-        # self.timer = self.create_timer(timer_period, self.sending_request)
+        # self.timer = None
 
     def sending_request(self):
         msg = Twist()
-        stop_msg=Twist()
+        stop_msg = Twist()
         stop_msg.linear.x=0.0
-        stop_msg.linear.y=0.0
-        stop_msg.linear.z=0.0
-        stop_msg.angular.x=0.0
-        stop_msg.angular.y=0.0
         stop_msg.angular.z=0.0
+        
         if self.direction.get('move') is not None:
-            msg.linear.x=float(moveBindings[self.direction['move']['direction']][0])
-            msg.linear.y=float(moveBindings[self.direction['move']['direction']][1])
-            msg.linear.z=float(moveBindings[self.direction['move']['direction']][2])
-            msg.angular.z=float(moveBindings[self.direction['move']['direction']][3])
+            direction_value = self.direction['move']['direction']
+       	    distance_value = float(self.direction['move']['distance'])
+            time_value = float(self.direction['move']['time'])
+            msg.linear.x=float(moveBindings[direction_value][0] * distance_value)
+            msg.angular.z=float(moveBindings[direction_value][3] * distance_value)
 
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg)
         time.sleep(1)
-
+	
         self.publisher_.publish(stop_msg)
         self.get_logger().info('Publishing stop: "%s"' % stop_msg)
         #self.destroy_node()
@@ -72,24 +66,11 @@ class MinimalPublisher(Node):
 
 
 def main(args=None):
-    operation={
-        'move':{
-            'direction':'up',
-            'distance':1,
-            'time':1
-        }
-    }
     rclpy.init(args=args)
-    minimal_publisher = MinimalPublisher(operation)
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+    minimal_publisher = MinimalPublisher()
+    rclpy.spin(minimal_publisher)  
     minimal_publisher.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
 
